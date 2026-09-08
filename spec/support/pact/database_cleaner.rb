dacd530be18caba0566b929d752991e98a4f0b17
Lucas Mbiwe <lucas.mbiwe@dsit.gov.uk>
2026-09-08 17:04:52 +0100

    Finalise Pact v2 migration
    - Bump gem version to ~> 2.0
    - Remove pact-ffi gem because pact v2 adds it implicitly
    - Add rspec gem because pact no longer adds but still requires it
    - Remove any redundant v2 references (the v2 namespace no longer exists)
    - Ensure CI calls pact_verify (w/o _v2 suffix)
    - Use signon's docker-compose file to feed DatabaseCleaner's allow list

--- spec/support/pact/database_cleaner.rb
+++ spec/support/pact/database_cleaner.rb
@@ -1,9 +1,18 @@
 require "database_cleaner/active_record"
 
 if ENV["DATABASE_URL"]
-  DatabaseCleaner.url_allowlist = [
-    %r{\Amysql2://root:root@mysql-8/signon_(?:development|test)\z},
-  ]
+  compose_file = Rails.root.join("../govuk-docker/projects/signon/docker-compose.yml")
+  raise "Cannot build DatabaseCleaner allow list; #{compose_file} not found" unless compose_file.exist?
+
+  compose_yaml = YAML.safe_load(File.read(compose_file), aliases: true)
+
+  database_urls = compose_yaml.fetch("services", {}).flat_map { |_name, service|
+    env = service["environment"] || {}
+    env = env.map { |key, val| "#{key}=#{val}" } if env.is_a?(Hash) # could be hash or list
+    env.grep(/\A(?:TEST_)?DATABASE_URL=/) { _1.split("=", 2).last }
+  }.uniq
+
+  DatabaseCleaner.url_allowlist = database_urls
 end
 
 DatabaseCleaner.strategy = :truncation
